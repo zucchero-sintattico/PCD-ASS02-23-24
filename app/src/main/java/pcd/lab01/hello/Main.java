54091145dae9326afbbbc0143335a31c53e024e9
Alessandro Ricci <a.ricci@unibo.it>
2024-02-26 08:22:07 +0100

    Lab #01

---
+++ app/src/main/java/pcd/lab01/hello/Main.java
@@ -0,0 +1,23 @@
+package pcd.lab01.hello;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		MyThread myThread = new MyThread("MySimpleThread");
+		myThread.start();		
+		
+		log("Thread spawned.");
+		log("Waiting for its termination.");
+		
+		myThread.join();
+
+		log("Completed.");
+		
+	}
+
+	private static void log(String msg) {
+		System.out.println("[ " + Thread.currentThread().getName()+ " ][ " + System.currentTimeMillis() + " ] " + msg); 
+	}
+	
+}
