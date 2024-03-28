ae8fc2d1020ebb22b21be4fe096d10cab4400b15
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-03-28 18:07:29 +0100

    fix file comparator

--- app/src/test/java/FileComparator.java
+++ app/src/test/java/FileComparator.java
@@ -1,17 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileComparator {
     public static boolean compareFiles(String path1, String path2) {
 
         try {
-            byte[] file1 = Files.readAllBytes(Paths.get(path1));
-            byte[] file2 = Files.readAllBytes(Paths.get(path2));
-
-
-            return Arrays.equals(file1, file2);
+            List<String> file1 = new BufferedReader(new FileReader(path1)).lines().toList();
+            List<String> file2 = new BufferedReader(new FileReader(path2)).lines().toList();
+            return file1.equals(file2);
         } catch (IOException e) {
             e.printStackTrace();
             return false;
