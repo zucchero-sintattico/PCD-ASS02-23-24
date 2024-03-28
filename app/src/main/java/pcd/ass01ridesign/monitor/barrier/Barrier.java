6f64fd70412356bad558b5d633551b63052c63e8
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-03-28 18:43:37 +0100

    barrier refactor

---
+++ app/src/main/java/pcd/ass01ridesign/monitor/barrier/Barrier.java
@@ -0,0 +1,7 @@
+package pcd.ass01ridesign.monitor.barrier;
+
+public interface Barrier {
+
+    void hitAndWaitAll() throws InterruptedException;
+
+}
---
+++ app/src/main/java/pcd/ass01ridesign/monitor/barrier/CyclicBarrier.java
@@ -0,0 +1,54 @@
+package pcd.ass01ridesign.monitor.barrier;
+
+import java.util.function.Consumer;
+
+public class CyclicBarrier implements Barrier {
+
+    private final int numberOfParticipants;
+    private int participants = 0;
+    private boolean broken;
+
+    public CyclicBarrier(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
+    @Override
+    public synchronized void hitAndWaitAll() throws InterruptedException {
+        this.enterBarrier();
+        while (!broken) {
+            wait();
+        }
+        this.passBarrier();
+    }
+
+    private void enterBarrier() throws InterruptedException {
+        while (broken) {
+            wait();
+        }
+       evaluateParticipants(this::breakBarrier);
+    }
+
+    private void passBarrier() {
+        evaluateParticipants(this::reset);
+    }
+
+    private void breakBarrier() {
+        broken = true;
+        participants = 0;
+        notifyAll();
+    }
+
+    private synchronized void reset() {
+        broken = false;
+        participants = 0;
+        notifyAll();
+    }
+
+    private void evaluateParticipants(Runnable action){
+        participants++;
+        if (participants == numberOfParticipants) {
+            action.run();
+        }
+    }
+
+}
