83847569b523677c6db9c19a05b39c628711a1ec
Alex Testa <alex.testa@studio.unibo.it>
2024-04-23 16:44:54 +0200

    Bug fix

--- app/src/main/scala/reactiveProgramming/model/utils/WebElement.scala
+++ app/src/main/scala/reactiveProgramming/model/utils/WebElement.scala
@@ -4,10 +4,10 @@ import org.jsoup.Jsoup
 import reactiveProgramming.model.utils.WebElement.WebElementImpl
 
 object WebElement:
-  case class WebElementImpl(private val url: String, private val word: String, private val depth: Int):
+  case class WebElementImpl(private val url: String, private val wordToFind: String, private val depth: Int):
     def connect(): (Int, java.util.List[String]) =
       val doc = Jsoup.connect(url).get()
-      val word = doc.html().trim.replaceAll("<[^>]*>", "").lines().map(_.toLowerCase).filter(_.contains("wikipedia")).count().toInt
+      val word = doc.html().trim.replaceAll("<[^>]*>", "").lines().map(_.toLowerCase).filter(_.contains(wordToFind)).count().toInt
       val links = doc.select("a").stream().map(_.attr("href")).filter(_.contains("https://")).skip(1).toList
       println(s"occorrenze trovate: $word\nin: $url")
       (word, links)
