9b87ac8f8ebbf9399bfd69f761b13bca50ebaeb4
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-05-07 16:09:26 +0200

    remove legacy dependency

--- app/src/main/java/part2/virtualThread/Main.java
+++ app/src/main/java/part2/virtualThread/Main.java
@@ -5,7 +5,6 @@ import part2.virtualThread.view.GUI;
 
 public class Main {
     public static void main(String[] args) {
-        org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
         Configuration.setup();
         new GUI().display();
     }
