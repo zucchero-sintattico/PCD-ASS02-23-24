74d93d10f5e9c64dfe1a2f119a1435ec106021bc
Federico Bravetti <federico.bravetti2@studio.unibo.it>
2024-05-07 10:50:17 +0200

    fix monitor stackOverflow and lostLog issues

--- app/src/main/java/part2/virtualThread/Main.java
+++ app/src/main/java/part2/virtualThread/Main.java
@@ -3,6 +3,8 @@ package part2.virtualThread;
 import part2.virtualThread.utils.Configuration;
 import part2.virtualThread.view.GUI;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class Main {
     public static void main(String[] args) {
         org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
--- app/src/main/java/part2/virtualThread/view/SearchInfo.java
+++ app/src/main/java/part2/virtualThread/view/SearchInfo.java
@@ -1,9 +1,12 @@
 package part2.virtualThread.view;
 
+import part2.virtualThread.state.LogBuffer;
 import part2.virtualThread.state.SearchState;
 
-public record SearchInfo(int totalPageRequested, int totalWordFound, int treadAlive, String newLog) {
+import java.util.concurrent.atomic.AtomicReference;
+
+public record SearchInfo(int totalPageRequested, int totalWordFound, int treadAlive, LogBuffer newLog) {
     public static SearchInfo from(SearchState state) {
-        return new SearchInfo(state.getLinkState().getLinkExplored().size(), state.getWordOccurrences(), state.getThreadAlive().size(), state.getNewLog());
+        return new SearchInfo(state.getLinkState().getLinkExplored().size(), state.getWordOccurrences(), state.getThreadAlive().size(), state.getLogs());
     }
 }
